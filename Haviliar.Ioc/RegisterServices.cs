7ce629079e86464675d5335cef7e005fb3418265
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-17 19:38:00 -0300

    chore: add some packages to register services

---
+++ Haviliar.Ioc/RegisterServices.cs
@@ -0,0 +1,27 @@
+﻿using ClientTrustScore.Ioc.Configurations;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Haviliar.Ioc;
+
+public static class Ioc
+{
+    public static string DbConnectionString { get; set; } = string.Empty;
+
+    public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+    {
+        DbConnectionString = string.Format(configuration.GetConnectionString("DefaultConnection")!,
+            configuration["DB_USER"], configuration["DB_PASSWORD"]);
+
+        services.AddScoped<SqlConnection>(_ =>
+        {
+            var conn = new SqlConnection(DbConnectionString);
+            conn.Open();
+            return conn;
+        });
+
+        services.AddDI();
+    }
+}
+
