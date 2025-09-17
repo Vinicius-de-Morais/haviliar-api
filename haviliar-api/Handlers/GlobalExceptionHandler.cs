854279f0dd10e717a0d2197c2b7760b75fb0e89b
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-17 19:58:10 -0300

    feat: add global handler exception

---
+++ haviliar-api/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using Haviliar.Domain.Shared;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace haviliar_api.Handlers;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var problemDetails = exception switch
+        {
+            DomainException => new ProblemDetails
+            {
+                Title = nameof(DomainException),
+                Detail = exception.Message,
+                Status = StatusCodes.Status400BadRequest
+            },
+            _ => new ProblemDetails
+            {
+                Title = "Erro interno de servidor",
+                Detail = exception.Message,
+                Status = StatusCodes.Status500InternalServerError
+            }
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status!.Value;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
