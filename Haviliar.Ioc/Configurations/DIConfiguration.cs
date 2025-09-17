b9c8000f534730888aa51f74a12e549e1230fadb
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-17 19:46:54 -0300

    feat: add di configuration

---
+++ Haviliar.Ioc/Configurations/DIConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Haviliar.Application;
+using Haviliar.Domain;
+using Haviliar.Infra;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Haviliar.Ioc.Configurations;
+
+public static class DIConfiguration
+{
+    public static void AddDI(this IServiceCollection services)
+    {
+        services.Scan(scan =>
+        {
+            scan.FromAssemblyOf<InfraContext>()
+                .AddClasses()
+                .AsImplementedInterfaces()
+                .WithScopedLifetime();
+
+            scan.FromAssemblyOf<DomainContext>()
+                .AddClasses(c =>
+                {
+                    c.Where(type => !typeof(Exception).IsAssignableFrom(type));
+                })
+                .AsImplementedInterfaces()
+                .WithScopedLifetime();
+
+            scan.FromAssemblyOf<ApplicationContext>()
+                .AddClasses()
+                .AsImplementedInterfaces()
+                .WithScopedLifetime();
+        });
+    }
+}
+
