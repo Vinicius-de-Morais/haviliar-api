82d2c095d6ac6d9203c314f15bd6fa7c09e87960
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-26 20:56:19 -0300

    feat: add entity to context

--- Haviliar.Infra/Context/AppDbContext.cs
+++ Haviliar.Infra/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Haviliar.Domain.Users.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Haviliar.Infra.Context
 {
@@ -9,6 +10,7 @@ namespace Haviliar.Infra.Context
 
         }
 
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
