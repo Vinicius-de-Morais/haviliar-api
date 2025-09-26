30f69adc7b74bb2da836ff388bece12052fc0464
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-26 20:38:20 -0300

    feat: implement repository base

---
+++ Haviliar.Infra/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using Haviliar.Domain;
+using Haviliar.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Haviliar.Infra;
+
+public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public RepositoryBase(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+
+    }
+
+    public Task<bool> AlreadyExistAsync(Expression<Func<T, bool>> filter, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> InsertAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
