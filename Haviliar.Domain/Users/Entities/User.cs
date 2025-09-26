83f9704ce2b2e556ab5c9840778504fc2c62c23f
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-26 20:14:58 -0300

    feat: add user entity

---
+++ Haviliar.Domain/Users/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Haviliar.Domain.Users.Enums;
+
+namespace Haviliar.Domain.Users.Entities;
+
+public class User
+{
+    public int UserId { get; set; }
+    public required string Email { get; set; }
+    public required string Document { get; set; }
+    public required string Address { get; set; }
+    public required string Phone { get; set; }
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+    public required DateOnly DateOfBirth { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public required UserTypeEnum UserType { get; set; }
+}
