e178ac53d970a560b096267208fc82a19c375465
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-26 20:39:09 -0300

    refactor: update implementation of repository base

--- Haviliar.Infra/RepositoryBase.cs
+++ Haviliar.Infra/RepositoryBase.cs
@@ -18,23 +18,29 @@ public class RepositoryBase<T> : IRepositoryBase<T> where T : class
 
     }
 
-    public Task<bool> AlreadyExistAsync(Expression<Func<T, bool>> filter, CancellationToken cancellationToken)
+    public async Task<bool> AlreadyExistAsync(Expression<Func<T, bool>> filtro, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await _dbSet.AnyAsync(filtro, cancellationToken);
     }
 
-    public Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    public async Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        _context.Update(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
     }
 
-    public Task<T> InsertAsync(T entity, CancellationToken cancellationToken = default)
+    public async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return await _dbSet.FindAsync(id, cancellationToken);
     }
 
-    public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+    public async Task<T> InsertAsync(T entity, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        await _dbSet.AddAsync(entity, cancellationToken);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity;
     }
 }
