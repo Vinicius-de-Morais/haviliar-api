ad0cbc357bb65a7a0068809faf4a9271b7994934
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-09-26 20:20:40 -0300

    feat: add base repository

---
+++ Haviliar.Domain/IRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace Haviliar.Domain;
+
+public interface IRepositoryBase<T> where T : class
+{
+    Task<bool> AlreadyExistAsync(Expression<Func<T, bool>> filter, CancellationToken cancellationToken);
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task<T> InsertAsync(T entity, CancellationToken cancellationToken = default);
+    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+}
