b6df69494ef26f50466f482a0b5de5e149eb1869
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 19:47:51 -0300

    feat: add exceptions for user

---
+++ Haviliar.Domain/Users/Exceptions/DocumentAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace Haviliar.Domain.Users.Exceptions;
+
+public class DocumentAlreadyExistsException : Exception
+{
+    public DocumentAlreadyExistsException() : base("CPF já cadastrado em outro cliente")
+    {
+
+    }
+}
---
+++ Haviliar.Domain/Users/Exceptions/EmailAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace Haviliar.Domain.Users.Exceptions;
+
+public class EmailAlreadyExistsException : Exception
+{
+    public EmailAlreadyExistsException() : base("E-mail já cadastrado em outro cliente")
+    {
+
+    }
+}
