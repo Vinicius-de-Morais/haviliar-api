2dbc2ba476dcc31159a7b3949f686e6a49eae139
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 20:01:29 -0300

    refactor: separte user address request

---
+++ Haviliar.DataTransfer/Users/Requests/UserAddressRequest.cs
@@ -0,0 +1,42 @@
+﻿namespace Haviliar.DataTransfer.Users.Requests;
+
+/// <summary>
+/// Dados do endereço do usuário.
+/// </summary>
+public record UserAddressRequest
+{
+    /// <summary>
+    /// Cidade do endereço.
+    /// </summary>
+    public required string City { get; init; }
+
+    /// <summary>
+    /// Estado ou região do endereço.
+    /// </summary>
+    public required string State { get; init; }
+
+    /// <summary>
+    /// Bairro do endereço.
+    /// </summary>
+    public required string Neighborhood { get; init; }
+
+    /// <summary>
+    /// Código postal do endereço.
+    /// </summary>
+    public required string ZipCode { get; init; }
+
+    /// <summary>
+    /// Rua do endereço.
+    /// </summary>
+    public required string Street { get; init; }
+
+    /// <summary>
+    /// Número do endereço.
+    /// </summary>
+    public required string Number { get; init; }
+
+    /// <summary>
+    /// Complemento do endereço.
+    /// </summary>
+    public string? Complement { get; init; }
+}
