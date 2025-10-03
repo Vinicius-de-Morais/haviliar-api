c74855d3498a2407547b451483590a7e8692441a
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 19:28:02 -0300

    feat: add pagination user request

---
+++ Haviliar.DataTransfer/Users/Requests/PaginationUserRequest.cs
@@ -0,0 +1,17 @@
+﻿using Haviliar.DataTransfer.Pagination.Requests;
+using Haviliar.Domain.Pagination.Enums;
+
+namespace Haviliar.DataTransfer.Users.Requests;
+
+public record class PaginationUserRequest : PaginationFilterRequest
+{
+    public PaginationUserRequest() : base(["Name"], [OrderDirectionEnum.Asc])
+    {
+
+    }
+
+    /// <summary>
+    /// Texto para busca, aplicável a Nome ou CPF do cliente
+    /// </summary>
+    public string? Search { get; set; }
+}
