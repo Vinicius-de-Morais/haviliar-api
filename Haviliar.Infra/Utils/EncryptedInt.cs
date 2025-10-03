2e09b4554ed1973aea4428b0a14fc068e91ddad7
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 19:42:52 -0300

    feat: add struct encryptedId and converter

---
+++ Haviliar.Infra/Utils/EncryptedInt.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Haviliar.Infra.Utils;
+
+[TypeConverter(typeof(EncryptedIntConverter))]
+public readonly struct EncryptedInt
+{
+    public int Value { get; }
+
+    public EncryptedInt(int value) => Value = value;
+
+    public static implicit operator int(EncryptedInt e) => e.Value;
+
+    public static implicit operator EncryptedInt(int value) => new(value);
+}
---
+++ Haviliar.Infra/Utils/EncryptedIntConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Haviliar.Infra.Utils;
+
+public class EncryptedIntConverter : TypeConverter
+{
+    public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+    {
+        return sourceType == typeof(int) || base.CanConvertFrom(context, sourceType);
+    }
+
+    public override object? ConvertFrom(
+        ITypeDescriptorContext? context,
+        System.Globalization.CultureInfo? culture,
+        object value
+    )
+    {
+        if (value is int intValue)
+        {
+            return new EncryptedInt(intValue);
+        }
+        return base.ConvertFrom(context, culture, value);
+    }
+}
