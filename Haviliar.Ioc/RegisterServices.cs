e9a5ece63f1b715b50abd0bcb922f7f58d142bfc
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 20:46:53 -0300

    refactor: ajusta para usar ef

--- Haviliar.Ioc/RegisterServices.cs
+++ Haviliar.Ioc/RegisterServices.cs
@@ -1,9 +1,12 @@
 ﻿using System.Data;
+using Haviliar.Infra.Context;
 using Haviliar.Ioc.Configurations;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Haviliar.Ioc;
 
@@ -16,12 +19,13 @@ public static class Ioc
         DbConnectionString = string.Format(configuration.GetConnectionString("DefaultConnection")!,
             configuration["DB_USER"], configuration["DB_PASSWORD"]);
 
-        services.AddScoped<IDbConnection>(_ =>
+
+        services.AddDbContext<AppDbContext>(options =>
         {
-            var conn = new NpgsqlConnection(DbConnectionString);
-            conn.Open();
-            return conn;
+            options.UseNpgsql(DbConnectionString);
         });
+
+        services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         services.AddDI();
     }
