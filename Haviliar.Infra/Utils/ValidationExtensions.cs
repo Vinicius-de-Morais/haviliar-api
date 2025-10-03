93f69c3f96b6f524f76c378cc50f5f94386cbbe2
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 20:31:45 -0300

    feat: add valitation extension

---
+++ Haviliar.Infra/Utils/ValidationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Haviliar.Infra.Utils;
+
+public static class ValidationExtensions
+{
+    public static IRuleBuilderOptions<T, string> MustBeCpf<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder.Must(value => value.IsCpf());
+    }
+
+    public static IRuleBuilderOptions<T, string> MustBePhoneNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder.Must(value => value.IsPhoneNumber());
+    }
+}
