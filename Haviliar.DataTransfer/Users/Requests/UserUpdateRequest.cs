e5df667b96406d59183dfa0e8fbaee12ea2829b9
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 20:01:05 -0300

    feat: add user update request

---
+++ Haviliar.DataTransfer/Users/Requests/UserUpdateRequest.cs
@@ -0,0 +1,44 @@
+﻿using Haviliar.Domain.Users.Enums;
+
+namespace Haviliar.DataTransfer.Users.Requests;
+
+/// <summary>
+/// Dados necessários para atualizar um usuário.
+/// </summary>
+public class UserUpdateRequest
+{
+    /// <summary>
+    /// Email do usuário.
+    /// </summary>
+    public required string Email { get; init; }
+
+    /// <summary>
+    /// Documento do usuário (CPF, RG, etc.).
+    /// </summary>
+    public required string Document { get; init; }
+
+    /// <summary>
+    /// Endereço do usuário.
+    /// </summary>
+    public required UserAddressRequest UserAddressRequest { get; init; }
+
+    /// <summary>
+    /// Telefone do usuário.
+    /// </summary>
+    public required string Phone { get; init; }
+
+    /// <summary>
+    /// Nome de usuário.
+    /// </summary>
+    public required string UserName { get; init; }
+
+    /// <summary>
+    /// Data de nascimento do usuário.
+    /// </summary>
+    public required DateOnly DateOfBirth { get; init; }
+
+    /// <summary>
+    /// Tipo do usuário (Admin, Regular, etc.).
+    /// </summary>
+    public required UserTypeEnum UserType { get; init; }
+}
