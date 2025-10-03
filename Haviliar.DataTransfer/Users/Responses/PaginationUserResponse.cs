2070eab78467cd66882afe7b47d37a9698a470cb
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 20:17:01 -0300

    feat: add responses for user

---
+++ Haviliar.DataTransfer/Users/Responses/PaginationUserResponse.cs
@@ -0,0 +1,34 @@
+﻿using Haviliar.Domain.Users.Enums;
+
+namespace Haviliar.DataTransfer.Users.Responses;
+
+/// <summary>
+/// Dados de resposta para paginação de usuários.
+/// </summary>
+public record PaginationUserResponse
+{
+    /// <summary>
+    /// Identificador único do usuário.
+    /// </summary>
+    public int UserId { get; set; }
+
+    /// <summary>
+    /// Nome de usuário.
+    /// </summary>
+    public required string UserName { get; set; }
+
+    /// <summary>
+    /// Data de criação do usuário.
+    /// </summary>
+    public required DateTime CreatedAt { get; set; }
+
+    /// <summary>
+    /// Data da última atualização do usuário.
+    /// </summary>
+    public DateTime UpdatedAt { get; set; }
+
+    /// <summary>
+    /// Tipo do usuário (Admin, Regular, etc.).
+    /// </summary>
+    public required UserTypeEnum UserType { get; set; }
+}
---
+++ Haviliar.DataTransfer/Users/Responses/UserResponse.cs
@@ -0,0 +1,59 @@
+﻿using Haviliar.Domain.Users.Enums;
+
+namespace Haviliar.DataTransfer.Users.Responses;
+
+/// <summary>
+/// Dados de resposta para um usuário.
+/// </summary>
+public record UserResponse
+{
+    /// <summary>
+    /// Identificador único do usuário.
+    /// </summary>
+    public int UserId { get; set; }
+
+    /// <summary>
+    /// Email do usuário.
+    /// </summary>
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// Documento do usuário (CPF).
+    /// </summary>
+    public required string Document { get; set; }
+
+    /// <summary>
+    /// Endereço do usuário.
+    /// </summary>
+    public required string Address { get; set; }
+
+    /// <summary>
+    /// Telefone do usuário.
+    /// </summary>
+    public required string Phone { get; set; }
+
+    /// <summary>
+    /// Nome de usuário.
+    /// </summary>
+    public required string UserName { get; set; }
+
+    /// <summary>
+    /// Data de nascimento do usuário.
+    /// </summary>
+    public required DateOnly DateOfBirth { get; set; }
+
+    /// <summary>
+    /// Data de criação do usuário.
+    /// </summary>
+    public required DateTime CreatedAt { get; set; }
+
+    /// <summary>
+    /// Data da última atualização do usuário.
+    /// </summary>
+    public DateTime UpdatedAt { get; set; }
+
+    /// <summary>
+    /// Tipo do usuário (Admin, Regular, etc.).
+    /// </summary>
+    public required UserTypeEnum UserType { get; set; }
+}
