edbd4fd01383ab398e3f2a165a18e8c6605a6a52
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 19:49:09 -0300

    feat: add user not found exception

---
+++ Haviliar.Domain/Users/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Haviliar.Domain.Users.Exceptions;
+
+public class ClientNotFoundException : Exception
+{
+    public ClientNotFoundException() : base("O usuário não foi encontrado.")
+    {
+
+    }
+}
