3bb3fac21bad65056a556ca0d927fdfdfd918ca2
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-10-03 19:27:50 -0300

    feat: add pagination filter request

---
+++ Haviliar.DataTransfer/Pagination/Requests/PaginationFilterRequest.cs
@@ -0,0 +1,44 @@
+﻿using Haviliar.Domain.Pagination.Enums;
+
+namespace Haviliar.DataTransfer.Pagination.Requests;
+
+/// <summary>
+/// Dados para filtragem e paginação de solicitações
+/// </summary>
+public record PaginationFilterRequest
+{
+    /// <summary>
+    /// Lista de campos para ordenação
+    /// </summary>
+    public IList<string> Sort { get; set; } = new List<string>();
+
+    /// <summary>
+    /// Direção da ordenação para cada campo: Ascendente| Descendete
+    /// </summary>
+    public IList<OrderDirectionEnum> Order { get; set; } = new List<OrderDirectionEnum>();
+
+    /// <summary>
+    /// Número de registros por página
+    /// </summary>
+    public int PerPage { get; set; }
+
+    /// <summary>
+    /// Número da página atual
+    /// </summary>
+    public int Page { get; set; }
+
+    public PaginationFilterRequest()
+    {
+        PerPage = 10;
+        Page = 0;
+    }
+
+    public PaginationFilterRequest(IList<string> sort, IList<OrderDirectionEnum> order)
+    {
+        Sort = sort;
+        Order = order;
+        PerPage = 10;
+        Page = 0;
+    }
+}
+
