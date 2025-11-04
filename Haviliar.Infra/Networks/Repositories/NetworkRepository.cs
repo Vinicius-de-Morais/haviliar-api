d7a86964df71e6454326427df0f19a134bd2654f
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-11-04 20:13:17 -0300

    feat: add network repository

---
+++ Haviliar.Infra/Networks/Repositories/NetworkRepository.cs
@@ -0,0 +1,12 @@
+﻿using Haviliar.Domain.Networks.Entities;
+using Haviliar.Domain.Networks.Repositories;
+using Haviliar.Infra.Context;
+
+namespace Haviliar.Infra.Networks.Repositories;
+
+public class NetworkRepository : RepositoryBase<Network>, INetworkRepository
+{
+    public NetworkRepository(AppDbContext context) : base(context)
+    {
+    }
+}
