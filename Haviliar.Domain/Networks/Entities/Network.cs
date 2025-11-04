3056774fcfa52f960428be6359bfac2a8d3679cf
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-11-04 20:04:10 -0300

    feat: network entity

---
+++ Haviliar.Domain/Networks/Entities/Network.cs
@@ -0,0 +1,11 @@
+﻿using Haviliar.Domain.OperationCenters.Entities;
+
+namespace Haviliar.Domain.Networks.Entities;
+
+public class Network
+{
+    public int NetworkId { get; set; }
+    public required string NetworkName { get; set; }
+    public int OperationCenterId { get; set; }
+    public virtual OperationCenter OperationCenter { get; set; } = null!;
+}
