6e5f8011a2349e8a3c6e6161f17ca18ddb1529fe
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-11-04 20:04:23 -0300

    feat: network filter page

---
+++ Haviliar.Domain/Networks/Repositories/Filters/NetworkFilter.cs
@@ -0,0 +1,8 @@
+﻿using Haviliar.Domain.Pagination.Entities;
+
+namespace Haviliar.Domain.Networks.Repositories.Filters;
+
+public class NetworkFilter : PaginationFilter
+{
+    public string? Search { get; set; }
+}
