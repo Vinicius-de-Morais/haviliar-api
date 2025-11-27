72ac9203c21c4faca30d3625ef99d5ab75b06098
Angelo Avelino <angeloavelino33211781@gmail.com>
2025-11-27 19:48:29 -0300

    feat: add endpoint device

---
+++ Haviliar.DataTransfer/Devices/Requests/DeviceCommandRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Haviliar.DataTransfer.Devices.Requests;
+
+public record DeviceCommandRequest
+{
+    public string Serial { get; init; }
+    public string Action { get; init; }
+}
---
+++ haviliar-api/Controllers/DeviceController.cs
@@ -0,0 +1,40 @@
+﻿namespace haviliar_api.Controllers;
+
+using System.Text.Json;
+using Haviliar.DataTransfer.Devices.Requests;
+using haviliar_api.MQTT;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DevicesController : ControllerBase
+{
+    private readonly MqttService _mqttService;
+
+    public DevicesController(MqttService mqttService)
+    {
+        _mqttService = mqttService;
+    }
+
+    [HttpPost("command")]
+    public async Task<IActionResult> SendCommand([FromBody] DeviceCommandRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Serial) ||
+            string.IsNullOrWhiteSpace(request.Action))
+        {
+            return BadRequest("Serial e action são obrigatórios.");
+        }
+
+        var topic = "esp32/open";
+
+
+        await _mqttService.PublishAsync(topic, "open");
+
+        return Ok(new
+        {
+            success = true,
+            topic
+        });
+    }
+}
+
